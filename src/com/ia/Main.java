9078194d9ba82cc23281d69bbcf7c2027c91a4cc
Bioanas <33870208+Bioanas@users.noreply.github.com>
2018-01-07 23:08:06 +0200

    Keywords and expressions

---
+++ src/com/ia/Main.java
@@ -0,0 +1,40 @@
+package com.ia;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// a mile is equal to 1.609344 kilometers
+        double kilometres = (100 * 1.609344);
+        int highscore = 50;
+        if (highscore == 50) {
+            System.out.println("This is an expression");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
